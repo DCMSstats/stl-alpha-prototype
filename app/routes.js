ba1f70e4243f0edb687f347335ceb03eac72ce61
Jarec <jarec.basham@madetech.com>
2024-04-30 11:32:00 +0100

    Added routing for renewal or non renewal path

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,12 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/renewal', function(request, response) {
+
+    var existing_type = request.session.data['renewingOrUpdating']
+    if (existing_type == "no"){
+        response.redirect("/personal-info/operating-model")
+    } else {
+        response.redirect("/personal-info/registration-number")
+    }
+})
