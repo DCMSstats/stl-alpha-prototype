a8548eca45a437b12b5c9930e5849cfeccfc0d8e
pinkkoala95 <emma.keeley@madetech.com>
2024-08-23 08:57:53 +0100

    Add compliance page basic content (WIP); add r3 journey start page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,13 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
-})
+  function renderContent() {
+    const isLoggedIn = true; // Replace with your condition
+
+    if (isLoggedIn) {
+      return <h1>Welcome, User!</h1>;
+    } else {
+      return <p>Please log in to view this content.</p>;
+    }
+  }
+});
